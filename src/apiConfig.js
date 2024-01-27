2c75d375dd5e3003e64111dd0885b12564e9f64d
Pol Fradera <83241376+pol-fradera@users.noreply.github.com>
2024-01-27 11:38:08 +0100

    Add files via upload

---
+++ src/apiConfig.js
@@ -0,0 +1,5 @@
+export const BASE_URL="https://brfenergi.se/iprog/old/group/67/";   // the DH2642 proxy server
+export const API_KEY="3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767";
+
+//TODO: export (not default) BASE_URL and API_KEY
+// then in other files you can write import {BASE_URL, API_KEY} from /src/apiConfig.js;
